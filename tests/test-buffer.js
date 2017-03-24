21d5f1536b3e29742e821fe2cd9ea389bd870f13
叶嘉雨 <jiayu.ye@ele.me>
2017-03-24 15:01:21 +0800

    Test buffer test

---
+++ tests/test-buffer.js
@@ -0,0 +1,60 @@
+let assert = require('assert');
+let Thrift = require('../thrift');
+
+it('simple invoking must be supported', done => {
+
+  /* Server */
+
+  let server = Thrift.createServer((thrift) => {
+    thrift.on('data', message => {
+      let { name, id, type, fields } = message;
+      assert.equal(name, 'test');
+      assert.equal(id, 1);
+      assert.equal(type, 'CALL');
+      assert.equal(fields.length, 1);
+      fields.forEach(item => {
+        let { id, type, value } = item;
+        assert.equal(id, 1);
+        assert.equal(type, 'STRING');
+        assert(value instanceof Buffer);
+        assert.equal(value.toString(), 'strbuf');
+      });
+      thrift.write({
+        name,
+        id,
+        type: 'REPLY',
+        fields: fields
+      });
+    });
+  }).listen();
+
+  /* Client */
+
+  let thrift = Thrift.connect(server.address());
+
+  thrift.write({
+    name: 'test',
+    type: 'CALL',
+    id: 1,
+    fields: [
+      { id: 1, type: 'STRING', value: Buffer.from('strbuf') }
+    ]
+  });
+
+  thrift.on('data', message => {
+    let { id, name, type, fields } = message;
+    assert.equal(id, 1);
+    assert.equal(name, 'test');
+    assert.equal(type, 'REPLY');
+    assert.equal(fields.length, 1);
+    fields.forEach(item => {
+      let { id, type, value } = item;
+      assert.equal(id, 1);
+      assert.equal(type, 'STRING');
+      assert(value instanceof Buffer);
+      assert.equal(value.toString(), 'strbuf');
+    });
+    done();
+  });
+
+});
